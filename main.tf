7490a9e0588f63d61cc7f0fee48faff2de448555
Luis Ledezma <luis.ledezma@gorillalogic.com>
2024-08-06 18:07:43 -0600

    Testing new version

--- main.tf
+++ main.tf
@@ -71,6 +71,8 @@ resource "aws_security_group" "allow_http" {
   vpc_id = aws_vpc.main.id
 
   egress {
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
