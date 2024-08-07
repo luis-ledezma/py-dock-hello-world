f703bc3fdc4eeb5476b1a133ffa7c5b1060f7fda
Luis Ledezma <luis.ledezma@gorillalogic.com>
2024-08-06 19:49:09 -0600

    Adding terraform lint

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+"""Basic Flask application that prints a message."""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Version 3'
+    """Prints hello world."""
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
